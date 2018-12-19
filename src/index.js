dd1a8b2827f0225eed5f1e13bcf26826d3de7409
kinsomicrote <kinsomicrote.inc@gmail.com>
2018-12-19 16:09:59 +0100

    removed async mode

--- src/index.js
+++ src/index.js
@@ -4,9 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const AsyncMode = React.unstable_AsyncMode;
-
-ReactDOM.render(<AsyncMode>
-    <App />
-  </AsyncMode>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
